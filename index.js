e25cfd4fddc7c7454ccbe742d36f11cdaa025d35
Hudson Mateque <72740537+Hudson512@users.noreply.github.com>
2025-04-02 01:22:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+const multer = require('multer');
 var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+const upload = multer({dest: 'uploads/'})
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res)=>{
+  const avatar = req.file;
 
+  res.send({
+    name: avatar.originalname,
+    type: avatar.mimetype,
+    size: avatar.size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
